cc31596dcef1f71f730ee418ae66452e0abb9839
Yellow Huang <yellow.huang@silkrode.com.tw>
2020-01-18 19:11:27 +0800

    新增文章

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,25 +2,48 @@ const express = require("express");
 const router = express.Router();
 const firbaseAdminDb = require("../connections/firebase_admins");
 
-// 分類的 firebase 路徑
+// firebase 路徑
 const categoriesRef = firbaseAdminDb.ref("/categories/");
+const articlesRef = firbaseAdminDb.ref("/articles/");
 
-router.get("/archives", function(req, res, next) {
+router.get("/archives", (req, res, next) => {
   res.render("dashboard/archives", { title: "Express" });
 });
 
-router.get("/article", function(req, res, next) {
-  res.render("dashboard/article", { title: "Express" });
+/**
+ * 文章管理
+ */
+// 新增文章頁面
+router.get("/article/create", (req, res, next) => {
+  // 取得所有分類
+  categoriesRef.once("value").then(snapshot => {
+    const categories = snapshot.val();
+    res.render("dashboard/article", { categories });
+  });
+});
+
+// 新增文章
+router.post("/article/create", (req, res, next) => {
+  const data = req.body;
+  const articleRef = articlesRef.push();
+  const key = articleRef.key;
+  const updateTime = Math.floor(Date.now() / 1000);
+  data.id = key;
+  data.updateTime = updateTime;
+  console.log(data);
+  articleRef.set(data).then(() => {
+    res.redirect("/dashboard/article/create");
+  });
 });
 
 /**
  * 類別管理
  */
-// 路徑
-router.get("/categories", function(req, res, next) {
+// 類別頁面
+router.get("/categories", (req, res, next) => {
   const msg = req.flash("info");
   // 取得資料
-  categoriesRef.once("value").then(function(snapshot) {
+  categoriesRef.once("value").then(snapshot => {
     const categories = snapshot.val();
     const resData = { categories, hasInfo: msg.length > 0, msg };
     res.render("dashboard/categories", resData);
@@ -28,7 +51,7 @@ router.get("/categories", function(req, res, next) {
 });
 
 // 新增類別
-router.post("/categories/create", function(req, res) {
+router.post("/categories/create", (req, res) => {
   const data = req.body;
 
   // 取得單一類別
@@ -40,13 +63,13 @@ router.post("/categories/create", function(req, res) {
     .orderByChild("path")
     .equalTo(data.path)
     .once("value")
-    .then(function(snapshot) {
+    .then(snapshot => {
       if (snapshot.val()) {
         req.flash("info", "已有相同路徑");
         res.redirect("/dashboard/categories");
       } else {
         data.id = key;
-        categoryRef.set(data).then(function() {
+        categoryRef.set(data).then(() => {
           res.redirect("/dashboard/categories");
         });
       }
@@ -54,7 +77,7 @@ router.post("/categories/create", function(req, res) {
 });
 
 // 刪除
-router.post("/categories/delete/:id", function(req, res) {
+router.post("/categories/delete/:id", (req, res) => {
   const id = req.params.id;
   categoriesRef.child(id).remove();
   req.flash("info", "欄位已刪除");
