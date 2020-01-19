1ee6a4f2b3ef869d0570702082aef5c47c1310ed
Yellow Huang <yellow.huang@silkrode.com.tw>
2020-01-19 21:20:35 +0800

    分頁功能

--- routes/index.js
+++ routes/index.js
@@ -24,13 +24,43 @@ router.get("/", function(req, res, next) {
       snapshot.forEach(item => {
         item.val().status === "public" && articles.push(item.val());
       });
-      articles.reverse();
-      console.log(articles, categories);
-      res.render("index", { categories, articles, stringtags, moment });
+      articles.reverse(); // 取新->舊
+      /**
+       * [ 分頁功能 ]
+       * - 總共有幾筆資料 totalResult
+       * - 總共有幾頁 totalPages
+       * - 每頁有幾筆 perpage
+       * - 目前在第幾頁 currentPage
+       * - 當前頁面資料
+       *  */
+      const totalResult = articles.length;
+      const perpage = 2; // 每頁有幾筆
+      const totalPages = Math.ceil(totalResult / perpage); // 無條件進位
+      let currentPage = Number.parseInt(req.query.page) || 1; // 當前頁數 不會比總頁數多
+      currentPage > totalPages && (currentPage = totalPages);
+
+      const minItem = currentPage * perpage - perpage + 1; //3
+      const maxItem = currentPage * perpage; //4
+
+      const resultData = [];
+      articles.forEach((item, i) => {
+        let num = i + 1;
+        num >= minItem && num <= maxItem && resultData.push(item);
+      });
+
+      // 畫面分頁需要的數值
+      const page = {
+        totalPages,
+        currentPage,
+        hasPre: currentPage > 1,
+        hasNext: currentPage < totalPages
+      };
+      res.render("index", { categories, articles: resultData, page, stringtags, moment });
     });
 });
 
 router.get("/post", function(req, res, next) {
+  res.render("index", { categories, articles, stringtags, moment });
   res.render("post", { title: "Express" });
 });
 
