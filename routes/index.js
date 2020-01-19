fb16e660f384f61d30659dcb97970eedee38e9cb
Yellow Huang <yellow.huang@silkrode.com.tw>
2020-01-19 19:29:01 +0800

    前台顯示文章列表

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,33 @@
-var express = require("express");
-var router = express.Router();
-var firbaseAdminDb = require("../connections/firebase_admins");
+const express = require("express");
+const router = express.Router();
+const firbaseAdminDb = require("../connections/firebase_admins");
+const stringtags = require("striptags");
+const moment = require("moment");
 
-const ref = firbaseAdminDb.ref("text");
-ref.once("value", function(snapshot) {
-  console.log(snapshot.val());
-});
+// firebase 路徑
+const categoriesRef = firbaseAdminDb.ref("/categories/");
+const articlesRef = firbaseAdminDb.ref("/articles/");
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "Express" });
+  let categories = {};
+  let articles = []; // 以陣列呈現 方便分頁製作
+  // 取得所有分類
+  categoriesRef
+    .once("value")
+    .then(snapshot => {
+      categories = snapshot.val();
+      // 取得所有文章
+      return articlesRef.once("value");
+    })
+    .then(snapshot => {
+      snapshot.forEach(item => {
+        item.val().status === "public" && articles.push(item.val());
+      });
+      articles.reverse();
+      console.log(articles, categories);
+      res.render("index", { categories, articles, stringtags, moment });
+    });
 });
 
 router.get("/post", function(req, res, next) {
